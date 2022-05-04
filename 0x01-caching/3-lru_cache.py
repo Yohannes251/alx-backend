4f933945bf5000e9bb65f74fc7d8a1719da83e17
Yohannes251 <yohannes.z251@gmail.com>
2022-05-04 22:34:03 +0000

    LRU cache replacement

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    This module defines a class that is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Implements LRU caching system that inherits from base_caching"""
+
+    def __init__(self):
+        """Initializes the class"""
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """Inserts items to cache and removes last inserted element
+        if number of elements exceeds maximum allowed number of items
+        """
+        if key is None or item is None:
+            return None
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            least_key = list(self.cache_data.keys())[0]
+            self.cache_data.pop(least_key)
+            print("DISCARD: {}".format(least_key))
+
+    def get(self, key):
+        """Returns requested item using provided key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        reappend_data = self.cache_data.pop(key)
+        self.cache_data[key] = reappend_data
+        return self.cache_data[key]
